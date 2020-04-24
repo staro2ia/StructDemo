caa393576eec6c53bd931cd30fa2105095cc6809
Paul <pasha.st@gmail.com>
2020-04-24 15:59:18 +1000

    Add input row,  edit row.

--- StructDemo/main.cpp
+++ StructDemo/main.cpp
@@ -36,6 +36,24 @@ struct Monastery {
 
 
 void editRow(Monastery* mon) {
+    cout << "Row is:" << endl;
+    cout << mon->name
+        << " | " << setw(6) << mon->type
+        << "  | " << setw(19) << mon->quantity
+        << "  | " << setw(19) << mon->square
+        << endl;
+    cout << "Input new values" << endl;
+    cout << "New name is " ;
+    cin >> mon->name;
+    
+    cout << "New type is " ;
+    cin >> mon->type;
+    
+    cout << "New quantity is " ;
+    cin >> mon->quantity;
+    
+    cout << "New square is " ;
+    cin >> mon->square;
     
 }
 
@@ -61,16 +79,18 @@ Monastery inputRow() {
 
 void printRow(const Monastery & mon) {
     cout << "| "  << mon.name
-        << " | " << setw(20) << mon.type
-        << " | " << setw(5) << mon.quantity
-        << " | " << setw(7) << mon.square
-        << " |" << endl;
+        << " | " << setw(6) << mon.type
+        << "  | " << setw(19) << mon.quantity
+        << "  | " << setw(19) << mon.square
+        << "  |" << endl;
 }
 
 void printHeader() {
     cout << "┌╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴┐\n"
     << "|Буддийские монастыри Японии периода                               |\n"
-    << "├╴╴╴╴╴╴╴╴╴╴┬╴╴╴╴╴╴╴╴╴┬╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴┬╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴┥\n";
+    << "├╴╴╴╴╴╴╴╴╴╴┬╴╴╴╴╴╴╴╴╴┬╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴┬╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴┥\n"
+    << "|Название  |  Школа  |  Количество монахов  |  Площадь земли (га)  |\n"
+    << "├╴╴╴╴╴╴╴╴╴╴┼╴╴╴╴╴╴╴╴╴┼╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴┼╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴╴┥\n";
 }
 
 void printFooter() {
@@ -91,8 +111,28 @@ void printTable(const Monastery* mons, size_t size) {
 int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
     
+    const size_t N = 10;
+    size_t count = 3;
+    
+    Monastery monasterys[N] = { { "Тодайдзи", 'T', 220, 368.8 },
+       { "Якусидзи", 'K', 50, 54.7 },
+       { "Дайандзи", 'D', 10, 12.2 }
+    };
+    
+    printTable(monasterys, count);
+    
+    monasterys[count] = inputRow();
+    count++;
+    
+    printTable(monasterys, count);
     
     
+    size_t num;
+    cout << "Input number row: ";
+    cin >> num;
+    editRow(&monasterys[num]);
+    
+    printTable(monasterys, count);
     
     return 0;
 }
