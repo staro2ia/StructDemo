dff7e79ced3417fb6af66d7c56b644e6806bef5d
Paul <pasha.st@gmail.com>
2020-04-24 15:03:13 +1000

    Initial Commit

---
+++ StructDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StructDemo
+//
+//  Created by Павел Тимош on 24.04.2020.
+//  Copyright © 2020 Павел Тимош. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
